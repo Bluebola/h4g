f54482ca0e4fa7ea5bac505183aba8f3b1dcc253
Timothy Lo <tlzw2003@gmail.com>
2024-02-05 19:16:59 +0800

    added the registerform input data function

--- src/components/pages/Register.js
+++ src/components/pages/Register.js
@@ -1,60 +1,89 @@
-import React from 'react';
+import React from "react";
 import {
   MDBInput,
   MDBCol,
   MDBRow,
   MDBCheckbox,
   MDBBtn,
-  MDBIcon
-} from 'mdb-react-ui-kit';
+  MDBIcon,
+} from "mdb-react-ui-kit";
+
 
 export default function Register() {
+  //This is the state variable that holds the user's info as they fill up the register form.
+  const [registerInfo, setRegisterInfo] = React.useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  });
+
+
+  //This function will update the registerInfo object everytime any of the inputs have changed.
+  function handleChange(event) {
+    setRegisterInfo((registerInfo) => {
+      return {
+        ...registerInfo,
+        [event.target.name]: event.target.value,
+      };
+    });
+  }
+  
+  console.log(registerInfo);
+
+  function handleClick(event) {
+    //send to database HERE
+  }
+
   return (
-    <div className='register--container'>
-    <form className='register--form'>
-      <MDBRow className='mb-4'>
-        <MDBCol>
-          <MDBInput id='form3Example1' label='First name' />
-        </MDBCol>
-        <MDBCol>
-          <MDBInput id='form3Example2' label='Last name' />
-        </MDBCol>
-      </MDBRow>
-      <MDBInput className='mb-4' type='email' id='form3Example3' label='Email address' />
-      <MDBInput className='mb-4' type='password' id='form3Example4' label='Password' />
+    <div className="register--container">
+      <form className="register--form">
+        <MDBRow className="mb-4">
+          <MDBCol>
+            <MDBInput
+              name="firstName"
+              id="form3Example1"
+              label="First name"
+              onChange={handleChange}
+            />
+          </MDBCol>
+          <MDBCol>
+            <MDBInput
+              name="lastName"
+              id="form3Example2"
+              label="Last name"
+              onChange={handleChange}
+            />
+          </MDBCol>
+        </MDBRow>
+        <MDBInput
+          name="email"
+          className="mb-4"
+          type="email"
+          id="form3Example3"
+          label="Email address"
+          onChange={handleChange}
+        />
+        <MDBInput
+          name="password"
+          className="mb-4"
+          type="password"
+          id="form3Example4"
+          label="Password"
+          onChange={handleChange}
+        />
 
-      <MDBCheckbox
-        wrapperClass='d-flex justify-content-center mb-4'
-        id='form3Example5'
-        label='Subscribe to our newsletter'
-        defaultChecked
-      />
+        <MDBCheckbox
+          wrapperClass="d-flex justify-content-center mb-4"
+          id="form3Example5"
+          label="Subscribe to our newsletter"
+          defaultChecked
+        />
 
-      <MDBBtn type='submit' className='mb-4' block>
-        Sign up
-      </MDBBtn>
-
-      <div className='text-center'>
-      
-        <p>or sign up with:</p>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='facebook-f' />
+        <MDBBtn type="submit" className="mb-4" block onClick = {handleClick} >
+          Sign up
         </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='google' />
-        </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='twitter' />
-        </MDBBtn>
-
-        <MDBBtn floating color="secondary" className='mx-1'>
-          <MDBIcon fab icon='github' />
-        </MDBBtn>
-      </div>
-    </form>
+      </form>
     </div>
   );
 }
